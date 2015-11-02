29a92568d0b934f3b83c482c61b987ab999b5a27
daisuke <daisuke.satou2@e-harp.jp>
2015-11-02 15:01:46 +0900

    Small Change2 for Direct Message

--- lib/message_handler.rb
+++ lib/message_handler.rb
@@ -10,13 +10,18 @@ module MessageHandler
 		text           = params[:micropost][:content]
 		recipient      = get_direct_message_recipient(text)
 		content        = trim_micropost_content(text)
-		direct_message = DirectMessage.create(sender_id: current_user.id, recipient_id: recipient.id, content: content)
-		if direct_message.save 
+
+		if recipient.nil?
+			redirect_to :back, 
+				flash: { error: "The opponent name is wrong." }
+		elsif content == ""
+			redirect_to :back, 
+				flash: { error: "Your DirectMessage has no content." }
+		else
+		    direct_message = DirectMessage.create(sender_id: current_user.id, recipient_id: recipient.id, content: content)
+		    direct_message.save 
 			redirect_to :back, 
 				flash: { success: "DirectMessage sent to #{recipient.user_name}."}
-		else
-			redirect_to :back, 
-				flash: { error: "An error has occured, your DirectMessage was not sent." }
 		end
 	end
 
@@ -25,6 +30,10 @@ module MessageHandler
 		def get_direct_message_recipient(text)
 			target_username = text.slice(/\Ad\s\b(\w|-|\.)+/i)[2..-1]
 			User.find_by(user_name: target_username)
+			#if User.name = User
+			#    redirect_to :back, 
+		#		flash: { error: "An error has occured, your DirectMessage was not sent." }
+	#		end
 		end
 
 		def trim_micropost_content(text)
